3be193802e91810b59081455ad0731a52fac1e18
Chaitanya-Soni <201951046@iiitvadodara.ac.in>
2021-05-15 13:18:10 +0530

    completed bubble sort function

--- sort.js
+++ sort.js
@@ -14,4 +14,16 @@ function bubblesort() {
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
     console.log(c);
+    let n=c.length;
+    for(let i=0;i<n-1;i++){
+        for(let j=0;j<n-i-1;j++){
+            if(c[j]>c[j+1]){
+                //swap function todo
+                let temp=c[j+1];
+                c[j+1]=c[j];
+                c[j]=temp;
+            }
+        }
+    } 
+    console.log("sorted ",c);
 }
