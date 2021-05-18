deff1c74c67a6369a2107b4b16414d740e2caa6a
Chaitanya-Soni <201951046@iiitvadodara.ac.in>
2021-05-18 20:21:15 +0530

    fix in canvas

--- sort.js
+++ sort.js
@@ -20,10 +20,34 @@ function getarray(){
         for(let i=0;i<n;i++){
             let wt=w;
             ctx.fillStyle = "black";
+            ctx.fillRect(i*w,600-(h*c[i]),100,h*c[i]);
+        }
+    }
+}
+function getRandomarray(){
+    let s = document.getElementById("input").value;
+    let c = s.split(" ").map(i => parseInt(i));
+    if (s[s.length-1] == " ")
+    {
+        alert("Do Not Enter Space at the end of array");
+    }
+    else{
+        document.getElementById("array_input").innerHTML = "Current array is: "+c;
+        let n=c.length;
+        let w=1000/n;
+        let h= 600;
+        let m=Math.max(...c);
+        h=h/m;
+        let can = document.getElementById("graph");
+        let ctx = can.getContext("2d");
+        for(let i=0;i<n;i++){
+            let wt=w;
+            ctx.fillStyle = "black";
             ctx.fillRect(i*w,0,100,h*c[i]);
         }
     }
 }
+
 function swap(){
 ;;    
 }
