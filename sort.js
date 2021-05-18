3a5a283f99381ee0e52fcf5bf0254bb0f4c40aff
Chaitanya-Soni <201951046@iiitvadodara.ac.in>
2021-05-18 12:52:39 +0530

    added canvas in html and added code for visualizing array in canvas

--- sort.js
+++ sort.js
@@ -1,4 +1,6 @@
-
+function buildgraph(){
+   ;
+}
 function getarray(){
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
@@ -6,10 +8,25 @@ function getarray(){
     {
         alert("Do Not Enter Space at the end of array");
     }
-    else
+    else{
         document.getElementById("array_input").innerHTML = "Current array is: "+c;
+        let n=c.length;
+        let w=1000/n;
+        let h= 600;
+        let m=Math.max(...c);
+        h=h/m;
+        let can = document.getElementById("graph");
+        let ctx = can.getContext("2d");
+        for(let i=0;i<n;i++){
+            let wt=w;
+            ctx.fillStyle = "black";
+            ctx.fillRect(i*w,0,100,h*c[i]);
+        }
+    }
 }
-
+function swap(){
+;;    
+}
 function bubblesort() {
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
