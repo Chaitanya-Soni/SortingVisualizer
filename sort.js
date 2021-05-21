0aed8b65c13fb7327839ea68a6f4fafedf9a5f0c
Shikhar30Shah <shikharshah30@gmail.com>
2021-05-21 19:09:14 +0530

    Made canvas design responsive

--- sort.js
+++ sort.js
@@ -1,6 +1,13 @@
 function buildgraph(){
    ;
 }
+
+var render = function () {
+    let ctr = document.getElementById("graph").getContext("2d");
+    ctr.canvas.width = document.documentElement.clientWidth*0.9;
+    ctr.canvas.height = document.documentElement.clientHeight*0.8;
+}
+
 function getarray(){
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
@@ -17,13 +24,17 @@ function getarray(){
         h=h/m;
         let can = document.getElementById("graph");
         let ctx = can.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        window.addEventListener("resize", render);
+        render();
         for(let i=0;i<n;i++){
-            let wt=w;
+            let wt=ctx.canvas.width/n;
             ctx.fillStyle = "black";
-            ctx.fillRect(i*w,600-(h*c[i]),100,h*c[i]);
+            ctx.fillRect(i*wt,ctx.canvas.height-(h*c[i]),ctx.canvas.width/(4*n),h*c[i]);
         }
     }
 }
+
 function getRandomarray(){
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
@@ -40,17 +51,25 @@ function getRandomarray(){
         h=h/m;
         let can = document.getElementById("graph");
         let ctx = can.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        window.addEventListener("resize", render);
+        render();
         for(let i=0;i<n;i++){
-            let wt=w;
+            let wt=ctx.canvas.width/n;
             ctx.fillStyle = "black";
-            ctx.fillRect(i*w,0,100,h*c[i]);
+            ctx.fillRect(i*wt,ctx.canvas.height-(h*c[i]),ctx.canvas.width/(4*n),h*c[i]);
         }
     }
 }
 
-function swap(){
-;;    
+function swap(a, b){
+    console.log(a + " " + b);
+    let temp = b;
+    b = a;
+    a = temp;
+    console.log(a + " " + b);
 }
+
 function bubblesort() {
     let s = document.getElementById("input").value;
     let c = s.split(" ").map(i => parseInt(i));
@@ -59,12 +78,11 @@ function bubblesort() {
     for(let i=0;i<n-1;i++){
         for(let j=0;j<n-i-1;j++){
             if(c[j]>c[j+1]){
-                //swap function todo
                 let temp=c[j+1];
                 c[j+1]=c[j];
                 c[j]=temp;
             }
         }
-    } 
+    }
     console.log("sorted ",c);
 }
